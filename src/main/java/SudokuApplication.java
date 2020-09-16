5bf6fe0a6842bc02a3dc19679fa180daeff85177
Szymon Gryszko <gryszkoszymon@gmail.com>
2020-09-16 21:48:06 +0200

    main, constants, UI

---
+++ src/main/java/SudokuApplication.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import userinterface.IUserInterfaceContract;
+import userinterface.UserInterfaceImpl;
+
+import java.io.IOException;
+
+
+public class SudokuApplication extends Application {
+
+    private IUserInterfaceContract.View uiImpl;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        uiImpl = new UserInterfaceImpl(primaryStage);
+        try {
+            SudokuBuildLogic.build(uiImpl);
+        }catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+
+    public static void main(String[] args) {launch(args);}
+}
---
+++ src/main/java/constants/Messages.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class Messages {
+    public static final String FAME_COMPLETE = "Congratulations! You won the game";
+    public static final String ERROR = "An error has occurred";
+}
